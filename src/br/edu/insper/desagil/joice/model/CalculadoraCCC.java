6e24ffc54b6041ec864da3ecd39653639fa19ffb
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2021-04-05 19:18:10 -0300

    Calculadoras prontas

--- src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -1,5 +1,20 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCCC {
-
+public class CalculadoraCCC extends Calculadora{
+	public CalculadoraCCC() {
+		super("Cúbica de Corpo Centrado");
+	}
+		
+	public double calcula(double peso, double raio) {
+		double massa;
+		double volume;
+		double densidade;
+		
+		raio *= Math.pow(10.0, -7.0);
+		massa = (peso*2)/(6.023*Math.pow(10, 23));
+		volume = Math.pow((4*raio)/Math.sqrt(3), 3);
+		densidade = massa/volume;
+		return densidade;
+		
+	}
 }
--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -1,5 +1,20 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCFC {
-
+public class CalculadoraCFC extends Calculadora{
+	public CalculadoraCFC() {
+		super("Cúbica de Face Centrada");
+	}
+		
+	public double calcula(double peso, double raio) {
+		double massa;
+		double volume;
+		double densidade;
+		
+		raio *= Math.pow(10.0, -7.0);
+		massa = (peso*4)/(6.023*Math.pow(10, 23));
+		volume = Math.pow((4*raio)/Math.pow(2, 0.5), 3);
+		densidade = massa/volume;
+		return densidade;
+		
+	}
 }
